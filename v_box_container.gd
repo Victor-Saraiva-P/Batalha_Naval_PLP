712ef742fee4d0866f8b859cde832148a70d4f40
Victor Alexandre Saraiva Pimentel <victoralex07062005@gmail.com>
2026-03-07 19:46:31 -0300

    Liga botão Jogar ao modo campanha e remove atalho C

--- v_box_container.gd
+++ v_box_container.gd
@@ -2,14 +2,10 @@ extends VBoxContainer
 
 func _ready():
 	$start.grab_focus()
+	if not $start.pressed.is_connected(_on_start_pressed):
+		$start.pressed.connect(_on_start_pressed)
 
 
 func _on_start_pressed():
-	print("pressionou start")
-
-
-func _unhandled_input(event):
-	if event is InputEventKey and event.pressed and not event.echo:
-		if event.keycode == KEY_C:
-			CampaignState.iniciar_nova_campanha()
-			get_tree().change_scene_to_file("res://scenes/modo_campanha.tscn")
+	CampaignState.iniciar_nova_campanha()
+	get_tree().change_scene_to_file("res://scenes/modo_campanha.tscn")
